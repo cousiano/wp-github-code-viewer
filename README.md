ba1fea45e910e55e6f2abb2ef20cf2b6c74c549c
cousiano <marc.rabahi@gmail.com>
2013-03-31 20:50:32 +0200

    add wp-github-code-viewer

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ wp-github-code-viewer
 =====================
 
 Wordpress github code viewer. That use geshi.
+
+Usage
+-----
+[gcv url='myGithubUrl' lang='geshiLang']
+
+*Example* 
+[gcv url='https://github.com/cousiano/wp-github-code-viewer/blob/master/wp-github-code-viewer.php' lang='php']
+
+
+Troubleshooting
+---------------
+Fatal error: Cannot redeclare class GeSHi in (...)
+You must desactivate others plugin who use GeSHi first.
+
+If you want to keep both plugin, you can comment the following line:
+```php
+include_once( 'geshi/geshi.php' );
+```
---
+++ wp-github-code-viewer.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: WP_Github_Code_Viewer
+Version: 1.0
+Plugin URI: https://github.com/cousiano/wp-github-code-viewer
+Description: Display raw file from github and display it using geshi syntax highliter. Usage [gcv url='myGithubUrl' lang='geshiLang']
+Author: Marc Rabahi <marc.rabahi@gmail.com>
+*/
+
+//
+// Include the GeSHi library
+//
+include_once( 'geshi/geshi.php' );
+
+class WP_Github_Code_Viewer {
+
+  // init plugin.
+	function init() {
+		add_shortcode('gcv', array(__CLASS__, '_add_shortcode'));
+	}
+
+  // add_shortcode
+	public function _add_shortcode($atts, $content = null) {
+		if (array_key_exists('url', $atts)) {
+			$url = $atts['url'];			
+		} else {
+			return self::getErrorFullMessage('did not find paramater <i>url</i>.');
+		}
+    
+    if (array_key_exists('lang', $atts)) {
+			$language = $atts['lang'];			
+		} else {
+			return self::getErrorFullMessage('did not find paramater <i>lang</i>.');
+		}		
+    
+    // get raw file from github
+    $source = file_get_contents($url . '?raw=true');
+    
+    // instanciate geshi
+    $geshi = new GeSHi($source, $language);
+    
+    // parameter geshi
+    $geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS, 2);
+    $geshi->set_line_style('background: #fcfcfc;', 'background: #f0f0f0;');
+    $geshi->set_code_style('font-size: 12px;');
+    
+    // parse code
+    return $geshi->parse_code();    
+	}
+  
+  // Return usage
+  private function usage()
+  {
+    $usage = "<b>Usage</b>: [gcv url='myGithubUrl' lang='geshiLang']";
+    $usage .="<br>";
+    $usage .="Example: [gcv url='https://github.com/cousiano/wp-github-code-viewer/blob/master/wp-github-code-viewer.php' lang='php']";
+    return $usage;
+  }
+  
+  // Return error string
+  private function getErrorFullMessage($errorMessage)
+  {
+    return "<b><font color='red'>$errorMessage</font></b><br><br>".self::usage();
+  }
+}
+
+WP_Github_Code_Viewer::init();
